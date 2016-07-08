e9a63fde6797cf1bb479b001f3089f4fdb1898e9
bujji <palakondubujji508@gmail.com>
2016-07-07 20:38:52 -0700

    added the new testdrive code fix 1234

---
+++ testdrive.java
@@ -0,0 +1,10 @@
+# this program is testdrive.java
+# this class is represent the testdrive string print
+# author : bujji
+class testdrive{
+       
+	public static void main(String args[]){
+		// this will print the test script
+		System.out.println("test script");
+		}
+	}
