730143181fc9097484e27f154797742d0783548d
jana <janardhanareddy.b@gmail.com>
2016-07-08 13:16:23 -0700

    added character

--- testdrive.java
+++ testdrive.java
@@ -1,10 +1,12 @@
-# this program is testdrive.java
-# this class is represent the testdrive string print
-# author : bujji
+// this program is testdrive.java
+//this class is represent the testdrive string print
+//author : bujji
 class testdrive{
        
 	public static void main(String args[]){
 		// this will print the test script
-		System.out.println("test script");
+		char[] ch={'m','a','a'};
+		
+		System.out.println("test script"+ch.toString());
 		}
 	}
