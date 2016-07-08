72d4fe8b6e4a6c95285554fe7196fc16b0bd38b1
bujji <palakondubujji508@gmail.com>
2016-07-08 13:02:31 -0700

    added string

--- testdrive.java
+++ testdrive.java
@@ -1,10 +1,11 @@
-# this program is testdrive.java
-# this class is represent the testdrive string print
-# author : bujji
+//this program is testdrive.java
+//this class is represent the testdrive string print
+//author : bujji
 class testdrive{
        
 	public static void main(String args[]){
 		// this will print the test script
-		System.out.println("test script");
+		String str="java";
+		System.out.println("test script " +str);
 		}
 	}
